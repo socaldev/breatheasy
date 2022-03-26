1ee3fbb10d181f0abc71f493114e9615f8b24b08
Devin Welch <6131305032@lamduan.mfu.ac.th>
2022-03-27 00:03:20 +0700

    improve Map UX

--- App.js
+++ App.js
@@ -1,54 +1,57 @@
-import * as React from 'react'
+import React, { Fragment } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import MainScreen from './screens/main.js'
 import InfoScreen from './screens/info.js'
 import MapScreen from './screens/map.js'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, StatusBar } from 'react-native'
 
 const Tab = createMaterialBottomTabNavigator()
 
 export default function App() {
 	return (
-		<SafeAreaView style={{ flex: 1, backgroundColor:'606569' }}>
-			<NavigationContainer>
-				<Tab.Navigator
-					initialRouteName='Home'
-					barStyle={{ backgroundColor: '#606569' }}
-				>
-					<Tab.Screen
-						name='Home'
-						component={MainScreen}
-						options={{
-							tabBarLabel: 'Home',
-							tabBarIcon: ({ color, size }) => (
-								<Icon name='home' color={color} size={25} />
-							),
-						}}
-					/>
-					<Tab.Screen
-						name='Map'
-						component={MapScreen}
-						options={{
-							tabBarLabel: 'Choose Location',
-							tabBarIcon: ({ color, size }) => (
-								<Icon name='map' color={color} size={25} />
-							),
-						}}
-					/>
-					<Tab.Screen
-						name='Info'
-						component={InfoScreen}
-						options={{
-							tabBarLabel: 'More Info',
-							tabBarIcon: ({ color, size }) => (
-								<Icon name='information' color={color} size={25} />
-							),
-						}}
-					/>
-				</Tab.Navigator>
-			</NavigationContainer>
-		</SafeAreaView>
+		<Fragment>
+			<StatusBar backgroundColor={'black'} />
+	
+				<NavigationContainer>
+					<Tab.Navigator
+						initialRouteName='Home'
+						barStyle={{ backgroundColor: '#606569' }}
+					>
+						<Tab.Screen
+							name='Home'
+							component={MainScreen}
+							options={{
+								tabBarLabel: 'Home',
+								tabBarIcon: ({ color, size }) => (
+									<Icon name='home' color={color} size={25} />
+								),
+							}}
+						/>
+						<Tab.Screen
+							name='Map'
+							component={MapScreen}
+							options={{
+								tabBarLabel: 'Choose Location',
+								tabBarIcon: ({ color, size }) => (
+									<Icon name='map' color={color} size={25} />
+								),
+							}}
+						/>
+						<Tab.Screen
+							name='Info'
+							component={InfoScreen}
+							options={{
+								tabBarLabel: 'More Info',
+								tabBarIcon: ({ color, size }) => (
+									<Icon name='information' color={color} size={25} />
+								),
+							}}
+						/>
+					</Tab.Navigator>
+				</NavigationContainer>
+
+			</Fragment>
 	)
 }
--- components/mapview.js
+++ components/mapview.js
@@ -2,9 +2,24 @@ import React, { useState } from 'react'
 import MapView, { Marker } from 'react-native-maps'
 import { View, Button } from 'react-native'
 
-const MyMapView = ( props ) => {
+const MyMapView = (props) => {
 	const { navigation } = props
-	const [ region, setRegion ] = useState(props.initialRegion)
+	const [region, setRegion] = useState(props.initialRegion)
+	const [marker, setMarker] = useState(props.initialRegion)
+	const handleOnPress = (e) => {
+		const { latitude, longitude } = e.nativeEvent.coordinate
+		setRegion((prevRegion) => ({
+			...prevRegion,
+			latitude,
+			longitude,
+		}))
+		setMarker({
+			latitude,
+			longitude,
+			latitudeDelta: region.latitudeDelta,
+			longitudeDelta: region.longitudeDelta,
+		})
+	}
 	return (
 		<View style={{ flex: 1 }}>
 			<MapView
@@ -14,16 +29,17 @@ const MyMapView = ( props ) => {
 				showsUserLocation={true}
 				onRegionChangeComplete={(region) => setRegion(region)}
 				navigation={navigation}
+				onPress={handleOnPress}
 			>
-				<Marker coordinate={region} />
+				<Marker coordinate={marker} />
 			</MapView>
 
 			<Button
 				title='Choose this location'
 				onPress={() => {
 					navigation.navigate('NewLocation', {
-						latitude: region.latitude,
-						longitude: region.longitude,
+						latitude: marker.latitude,
+						longitude: marker.longitude,
 					})
 				}}
 			/>
